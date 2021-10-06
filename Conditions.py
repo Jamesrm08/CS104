58994446478d41928706ef51410df80ea4b3315e
Jamesrm08 <91150820+Jamesrm08@users.noreply.github.com>
2021-10-06 08:50:25 -0400

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,25 @@
+temp=0
+
+temp= int(input('Please enter the current temperature:  '))
+
+#when a certain temp is inputted.
+
+#while temp != 999:
+
+if temp >= 90:
+    print ('Wear shorts')
+
+elif temp >= 70:
+    print ('Short sleeves are fine')
+
+elif temp >= 50:
+    print ('Wear a jacket')
+
+elif temp >= 32:
+    print ('Wear a heavy coat')
+
+elif temp < 32:
+    print('Stay inside')
+
+
+
